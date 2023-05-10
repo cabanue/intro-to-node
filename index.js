06260bf3337e5bf41c94c6aaa66f5b473e45779c
cabanue <70549810+cabanue@users.noreply.github.com>
2023-05-10 17:16:51 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
+
+var msh = superheroes.random();
+var msv = supervillains.random();
+
+console.log(msh + " vs. " + msv);
